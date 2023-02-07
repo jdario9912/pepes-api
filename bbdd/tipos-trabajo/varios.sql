c71a6f5630f97a57e41e78d7818c52cd1240234c
Joel Dario <joeldario9912@gmail.com>
2023-02-07 00:01:20 -0300

    cree tabla varios

---
+++ bbdd/tipos-trabajo/varios.sql
@@ -0,0 +1,34 @@
+CREATE TABLE varios (
+  id INT(4) NOT NULL AUTO_INCREMENT,
+  id_cliente INT NOT NULL,
+  nro_orden INT NOT NULL,
+  atendido_por VARCHAR(50),
+  fecha_entrega VARCHAR(10),
+  hora_entrega VARCHAR(6),
+  muestra VARCHAR(2),
+  tipo_trabajo VARCHAR(20) DEFAULT 'varios',
+  detalle VARCHAR(255),
+  observaciones VARCHAR(255),
+  total INT(7),
+  entrega INT(7),
+  estado VARCHAR(20),
+  PRIMARY KEY (id),
+  FOREIGN KEY(id_cliente) REFERENCES clientes(id)
+  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO varios (
+  id_cliente,
+  nro_orden,
+  atendido_por,
+  fecha_entrega,
+  hora_entrega,
+  muestra,
+  detalle,
+  observaciones,
+  total,
+  entrega,
+  estado
+) VALUES
+(3, 52, 'Jacinto', '13/04/2023', '19:00', 'si', 'Fotocopias color', '', 200, 200, 'pendiente'),
+(4, 53, 'Jacinto', '13/04/2023', '19:00', 'si', 'Fotocopias color', '', 200, 200, 'entregado');
