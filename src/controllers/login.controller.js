78897180284ec5ffb09e6c1b837e21040dda314d
Joel Dario <joeldario9912@gmail.com>
2023-02-09 00:56:26 -0300

    agregue validaciones al login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,18 +3,28 @@ const pool = require('../db');
 // POST
 const login = async (req, res) => {
   const { nickname, password } = req.body;
+
+  if(nickname === "" || password === "")
+    return res.status(404).json({ mensaje: 'No se puede ejecutar la petición'})
+  ;
   
   try {
     const [rows] = await pool.query(
-      `SELECT nickname, password FROM empleados WHERE nickname = ? AND password = ?`,
-      [nickname, password]
+      `SELECT nickname, password, permisions FROM empleados WHERE nickname = ?`,
+      [nickname]
     );
     
     if (rows.length == 0)
-      return res.json({ auth: false })
-    ;
+      return res.json({ mensaje: 'El usuario no existe', auth: false, permisions: false });
+    else{
+      if(password !== rows[0].password)
+        return res.json({ mensaje: 'Contraseña incorrecta', auth: false, permisions: false });
+    };
 
-    res.json({ auth: true });
+    if(rows[0].permisions === 'user')
+      return res.json({ mensaje: 'Login ok', auth: true, permisions: false });
+
+    res.json({ mensaje: 'Login ok', auth: true, permisions: true });
 
   } catch (error) {
     return res.status(500).json({ 
