bbc4a5ca5800ecd3ce4ca7306015b2037c39a993
Joel Dario <joeldario9912@gmail.com>
2023-02-18 01:25:36 -0300

    modifique tipo de dato numero de orden en tabla impresiones

--- src/controllers/impresiones.controller.js
+++ src/controllers/impresiones.controller.js
@@ -4,6 +4,8 @@ const mjsError = require('../models/mjs.error');
 const crearOrden = async (req, res) => {
   const { id_cliente, nro_orden, fecha_creacion, atendido_por, fecha_entrega, hora_entrega, muestra, ubicacion_archivo, faz, tipo_papel, tamano_papel, orientacion, anillado, abrochado, corte, observaciones, total, entrega, estado } = req.body;
 
+  console.log(estado);
+
   const datos = [fecha_entrega, hora_entrega, muestra, ubicacion_archivo, faz, tipo_papel, tamano_papel, orientacion, anillado, abrochado, corte, total, entrega, estado];
   
   datos.forEach(element => {
@@ -43,10 +45,9 @@ const crearOrden = async (req, res) => {
       return res.json({ registro: false, mensaje: `No se pudo realizar el registro`})
     }  
   } catch (error) {
+    console.log(error);
     res.status(500).json(mjsError);
   }
-  
-  res.json();
 };
 
 module.exports = {
