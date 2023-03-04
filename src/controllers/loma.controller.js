03344874c167e5a543fc71a7b97590d234a8b9a9
Joel Dario <joeldario9912@gmail.com>
2023-03-04 16:23:10 -0300

    modifique controlador crear orden tipo loma

--- src/controllers/loma.controller.js
+++ src/controllers/loma.controller.js
@@ -4,9 +4,9 @@ const handleError = require('../services/handleError');
 const camposVacios = require('../services/valida.campos');
 
 const crearOrden = async (req, res) => {
-  const {id_cliente, nro_orden, fecha_creacion, atendido_por, fecha_entrega, hora, muestra, ubicacion_archivo, material, orientacion, bolsillo, corte, ojales, troquelado, portabaner, observaciones, total, entrega, estado} = req.body;
-  const datos = [parseInt(id_cliente), parseInt(nro_orden), fecha_creacion, atendido_por, fecha_entrega, hora, muestra, ubicacion_archivo, material, orientacion, bolsillo, corte, ojales, troquelado, portabaner, observaciones, parseInt(total), parseInt(entrega), estado];
-  const campos = [fecha_entrega, hora, muestra, ubicacion_archivo, orientacion, corte, ojales, troquelado, portabaner];
+  const {id_cliente, nro_orden, fecha_creacion, atendido_por, fecha_entrega, hora_entrega, muestra, ubicacion_archivo, material, orientacion, bolsillo, corte, ojales, troquelado, portabaner, observaciones, total, entrega, estado} = req.body;
+  const datos = [parseInt(id_cliente), parseInt(nro_orden), fecha_creacion, atendido_por, fecha_entrega, hora_entrega, muestra, ubicacion_archivo, material, orientacion, bolsillo, corte, ojales, troquelado, portabaner, observaciones, parseInt(total), parseInt(entrega), estado];
+  const campos = [fecha_entrega, hora_entrega, muestra, ubicacion_archivo, orientacion, corte, ojales, troquelado, portabaner];
 
   if(camposVacios(campos)) return res.json({ registrado: false, mensaje: 'Hay datos sin completar'});
 
