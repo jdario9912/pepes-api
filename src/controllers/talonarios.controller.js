5d993cf6054942bfd11f8f7dc0dd72dc7a420455
Joel Dario <joeldario9912@gmail.com>
2023-03-09 17:43:17 -0300

    corregi dato de ubicacion de logo y color triplicado

--- src/controllers/talonarios.controller.js
+++ src/controllers/talonarios.controller.js
@@ -7,7 +7,7 @@ const { parsearValoresTalonarios } = require('../services/parseo.valores');
 const crearOrden = async (req, res) => {
   const {id_cliente, nro_orden, fecha_creacion, atendido_por, fecha_entrega, hora_entrega, muestra, tipo, cantidad, tamano, modelo_anterior, tiene_logo, ubicacion_logo, numero_desde, puntillado_emblocado, color_duplicado, triplicado, color_triplicado, observaciones, total, entrega, estado, abono} = req.body;
   const datos = [parseInt(id_cliente), parseInt(nro_orden), fecha_creacion, atendido_por, fecha_entrega, hora_entrega, muestra, tipo, parseInt(cantidad), tamano, modelo_anterior, tiene_logo, ubicacion_logo, parseInt(numero_desde), puntillado_emblocado, color_duplicado, triplicado, color_triplicado, observaciones, parseInt(total), parseInt(entrega), estado, abono];
-  const campos = [fecha_entrega, hora_entrega, muestra, cantidad, modelo_anterior, tiene_logo, numero_desde, puntillado_emblocado, triplicado, total, entrega];
+  const campos = [fecha_entrega, hora_entrega, muestra, cantidad, modelo_anterior, tiene_logo, ubicacion_logo, numero_desde, puntillado_emblocado, triplicado, total, entrega];
 
   if(camposVacios(campos)) return res.json({ registrado: false, mensaje: 'Hay datos sin completar'});
 
